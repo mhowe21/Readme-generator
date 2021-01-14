cb50b6b8b3eba48afa6d5a2bcb7fdc78760dda4c
Mason A. Howe <mason.a.howe@gmail.com>
2021-01-14 16:33:51 -0700

    add class

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,25 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const markdownGenerator =  require("./utils/generateMarkdown.js")
+const markdown = require("./utils/generateMarkdown.js");
+
+const questions = [
+    {
+        type: "input",
+        name: "readmeName",
+        message: "What is the name of the readme"
+
+    }
+]
 
 
-console.log(markdownGenerator)
-// TODO: Create an array of questions for user input
-const questions = [];
+
+inquirer.prompt(questions).then((answers) => {
+    //console.log(getData(answers))
+    let m = new markdown(answers)
+    console.log(m.print())
+})
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
