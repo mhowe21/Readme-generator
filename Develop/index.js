a43590e86387593a215c0fdd87a8029a734cb684
Mason A. Howe <mason.a.howe@gmail.com>
2021-01-15 22:53:54 -0700

    make file draft

--- Develop/index.js
+++ Develop/index.js
@@ -5,48 +5,7 @@ const { normalize } = require("path");
 const markdown = require("./utils/generateMarkdown.js");
 
 // selections
-const selections = [
-  {
-    type: "checkbox",
-    name: "selections",
-    message: "Select the sections you want added to your Readme",
-    choices: [
-      {
-        name: "Header",
-      },
-      {
-        name: "Description",
-      },
-      {
-        name: "Table of Contents",
-      },
-      {
-        name: "Installation Instructions",
-      },
-      {
-        name: "Usage Instructions",
-      },
-      {
-        name: "Credits",
-      },
-      {
-        name: "License",
-      },
-      {
-        name: "Badges",
-      },
-      {
-        name: "Features",
-      },
-      {
-        name: "Contributing",
-      },
-      {
-        name: "Tests",
-      },
-    ],
-  },
-];
+
 
 const questions = [
   {
@@ -56,48 +15,54 @@ const questions = [
   },
   {
     type: "input",
-    name: "description",
+    name: "Disc",
     message: "enter a description for your readme",
   },
 
   {
-    type: "editor",
+    type: "input",
     name: "Install",
     message: "Enter any instilation instructions",
   },
   {
-    type: "editor",
+    type: "input",
     name: "Usage",
     message: "Enter any usage instructions",
   },
   {
     type: "input",
-    name: "license",
-    message: "What Software License are you using?",
-    default: "None",
+    name: "Credits",
+    message: "Enter Any credits",
   },
   {
-    type: "list",
-    name: "tableOfContents",
-    message: "Would you like a table of contents?",
-    default: "No",
-    choices: ["Yes", "No"],
-  },
+      type: "list",
+      name: "License",
+      message: "What License type",
+      default: "None",
+
+      choices: 
+      [
+          "MIT",
+          "GNU",
+          "Apache",
+          "BSD",
+          "Other",
+          "None"
+      ]
+  }
 ];
 
-inquirer.prompt(selections).then((anwsers) => {
-  console.log(anwsers.selections);
-  
-});
 
-// inquirer.prompt(questions).then((answers) => {
-//     let fileName = JSON.stringify(answers.fileName)
-//     let m = new markdown(answers)
-//     let mark = m.generateMarkdown()
-//     console.log(m.print())
-//     //writeToFile(fileName, mark)
 
-// })
+inquirer.prompt(questions).then((answers) => {
+    
+    let m = new markdown(answers)
+    m.renderLicenseLink()
+    let mark = m.generateMarkdown()
+    console.log(m.print())
+    writeToFile("testReadme", mark)
+
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,19 +4,35 @@
 class markdown{
   constructor(data){
     this.data = data;
+    this.licenseInfo; 
   }
 
   print(){
     return(JSON.stringify(this.data))
   }
   renderLicenseBadge(license) {
-
+    
   }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-renderLicenseLink(license) {
-
+renderLicenseLink() {
+  let license = this.data.License;
+  let licenseLink = ``
+  if(license == "BSD"){
+    licenseLink = `[BSD License](https://opensource.org/licenses/BSD-3-Clause)`
+  }else if(license == "Apache"){
+    licenseLink = `[Apache License](https://www.apache.org/licenses/LICENSE-2.0)`
+  }else if(license == "GNU"){
+    licenseLink = `[GNU License](https://www.gnu.org/licenses/gpl-3.0.en.html)`
+  }else if(licenseLink == "MIT"){
+    licenseLink = `[MIT](https://opensource.org/licenses/MIT)`
+  }else if(licenseLink == "Other"){
+    licenseLink = "Other"
+  }else{
+    licenseLink = "None"
+  }
+  this.licenseInfo = `license /n${licenseLink}`
 }
 
 
@@ -28,7 +44,19 @@ renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 generateMarkdown() {
-  return `# ${this.data.title}
+  const{title,Disc,Install,Usage,Credits} = this.data
+  return `
+  # ${title}<br>
+  ## Discription <br>
+  ${Disc}<br>
+  ## Install Instructions <br>
+  ${Install}
+  ## Usage
+  ${Usage}<br>
+  ## License<br>
+  ${this.licenseInfo}<br>
+
+
 
 `;
 }
